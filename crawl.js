2d86f802ed5095081bd135798accb2f9df5d7876
NamLP <lpnam0201@gmail.com>
2020-01-22 22:26:13 +0700

    refactor crawl steps

--- crawl.js
+++ crawl.js
@@ -2,34 +2,37 @@ const https = require('https');
 const fs = require('fs');
 const cheerio = require('cheerio');
 
-function getUrlsToCrawl() {
+function getStickerImageUrls(pageHtml) {
+    let $ = cheerio.load(pageHtml);
+    let dataPreview_liTags = $('.FnStickerPreviewItem')
+    let dataPreviews = dataPreview_liTags.toArray().map(
+        li => JSON.parse($(li).attr('data-preview')));
+    dataPreviews.forEach(item => {
+        console.log(item.animationUrl);
+    });
+}
+
+function readMainPageUrls() {
     fs.readFile("urls.txt", 'utf-8', (error, data) => {
         let urls = data.split('\r\n');
 
         urls.forEach(url => {
-
-            https.get(`https://${url}`, res => {
-                let data = '';
-                res.on('data', chunk => {
-                    data += chunk;
-                })
-
-                res.on('end', () => {
-                    let $ = cheerio.load(data);
-                    let dataPreview_liTags = $('.FnStickerPreviewItem')
-                    let dataPreviews = dataPreview_liTags.toArray().map(
-                        li => JSON.parse($(li).attr('data-preview')));
-                    dataPreviews.forEach(item => {
-                        console.log(item.animationUrl);
-                    });
-                })
-            })
+            getMainPageHtml(url);
         });
     });
 }
 
-getUrlsToCrawl();
-let options = {};
+function getMainPageHtml(url) {
+    https.get(`https://${url}`, res => {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        })
 
+        res.on('end', () => {
+            getStickerImageUrls(data);
+        })
+    });
+}
 
-// http.get()
+readMainPageUrls();
