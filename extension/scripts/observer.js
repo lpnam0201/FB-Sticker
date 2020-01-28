024c515da58c57f45d916c7867d16b07769b4928
NamLP <lpnam0201@gmail.com>
2020-01-28 22:51:45 +0700

    append sticker tab to sticker scroll bar

--- extension/scripts/observer.js
+++ extension/scripts/observer.js
@@ -24,32 +24,43 @@ function htmlToElement(html) {
     template.innerHTML = html;
     return template.content.firstChild;
 }
-var appended = false;
+
 function initialize() {
+    let isStickerTabsAppended = false;
     let chatTabObserver = new MutationObserver((mutationList, observer) => {
         let stickersContainerDiv = document.querySelector('div[aria-label=Stickers] > div > div > div > div');
-        if (stickersContainerDiv && !appended) {
-            appended = true;
-            let stickerTabContainerHtml = buildStickerTabContainerHtml(stickerGroups);
-            let element = htmlToElement(stickerTabContainerHtml);
-            stickersContainerDiv.appendChild(element);
+        if (stickersContainerDiv !== null && !isStickerTabsAppended) {
+            if (!isStickerTabsAppended) {
+                let stickerTabContainerHtml = buildStickerTabContainerHtml(stickerGroups);
+                let element = htmlToElement(stickerTabContainerHtml);
+                stickersContainerDiv.appendChild(element);
+                isStickerTabsAppended = true;
+            }
+        } 
+
+        if (stickersContainerDiv === null) {
+            isStickerTabsAppended = false;
         }
     });
+    
+    observeForStickerPopup(chatTabObserver);
+}
+
+function observeForStickerPopup(mutationObserver) {
     let config = {
         attributeFilter: [],
         childList: true, 
         subtree: true 
     };
     
-    // Only what chat tabs container to avoid too many mutation being detected
+    // Only watch chat tabs container to avoid too many mutation being detected
     var chatTabContainer = document.getElementsByClassName('_59v1')[0];
     if (chatTabContainer) {
-        chatTabObserver.observe(chatTabContainer, config);
+        mutationObserver.observe(chatTabContainer, config);
     } else {
         // Is on messenger page
         // TODO on messenger page
     }
-    
 }
 
 initialize();
