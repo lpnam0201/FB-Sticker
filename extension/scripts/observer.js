a0685b8ae2ad800ebf9d093eaa49abe867ac6b7f
NamLP <lpnam0201@gmail.com>
2020-01-29 13:40:04 +0700

    should show extension inserted stickers upon clicking sticker tab

--- extension/scripts/observer.js
+++ extension/scripts/observer.js
@@ -1,4 +1,14 @@
 
+function chunk (arr, len) {
+    var chunks = [], i = 0, n = arr.length;
+  
+    while (i < n) {
+      chunks.push(arr.slice(i, i += len));
+    }
+  
+    return chunks;
+}
+
 function createStickerTabContainerElement(stickerGroups) {
     let html = Mustache.render(stickerTabContainerTemplate);
     let tabContainerElement = htmlToElement(html);
@@ -19,13 +29,36 @@ function createStickerTabElement(stickerGroup) {
     });
 
     let tabElement = htmlToElement(html);
-    //tabElement.addEventListener('click', )
+    tabElement.addEventListener('click', () => {
+        let tableElement = createStickersTableElement(stickerGroup);
+        let tableContainerElement = document.querySelector(
+            'div[aria-label=Stickers] > div:nth-child(2) > div');
+
+        // FB's default container should NEVER be removed (React error)
+        // but should be pushed down out of view
+        tableContainerElement.prepend(tableElement);
+        // For MutationObserver to decide the current tab is extenstion-inserted or normal
+        tableContainerElement.setAttribute('data-id', '');
+    });
+
     return tabElement;
 }
 
-function insertStickersTableElement(groupId) {
-    let group = stickerGroups.find(g => g.groupId === groupId);
-    let tableElement = Mustache.render()
+function createStickersTableElement(stickerGroup) {  
+    let table = { rows: [] };
+    let chunks = chunk(stickerGroup.stickers, 4);
+    for (let chunk of chunks) {
+        let row = { cells: []};
+        for (let sticker of chunk) {
+            let cell = { thumbnail: sticker.thumbnail };
+            row.cells.push(cell);
+        }
+        table.rows.push(row);
+    }
+
+    let tableElement = htmlToElement(Mustache.render(stickerTableTemplate, table));
+
+    return tableElement;
 }
 
 function htmlToElement(html) {
@@ -35,24 +68,91 @@ function htmlToElement(html) {
     return template.content.firstChild;
 }
 
+function chatTabContainerMutationHandler(mutationList, observer) {
+
+}
+
+function isStickerTableOfExtensionAdded(mutationList) {
+    for (let mutation of mutationList) {
+        let nodes = mutation.addedNodes.values();
+        for (let node of nodes) {
+            if (node.classList.contains('fbExtension')) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function stickerTableContainerMutationHandler(mutationList, observer) {
+    let stickerTableContainerElement = document.querySelector(
+        'div[aria-label=Stickers] > div:nth-child(2) > div');
+    
+    let tablesAddedByExtension = document.querySelectorAll('.fbExtension')
+    
+    // watch for data-id so there should be only one change at a time
+    let mutationRecord = mutationList[0];
+    let node = mutationRecord.target;
+
+    // #1 extension tab -> normal tab
+    if (!mutationRecord.oldValue && node.getAttribute('data-id')) {
+        // remove all extension-inserted tables (if any) to make room for normal table
+        tablesAddedByExtension.forEach(table => stickerTableContainerElement.removeChild(table));
+    } 
+    // #2 extension tab -> extension tab
+    if (!mutationRecord.oldValue && !node.getAttribute('data-id')) {
+        // extension tab -> extension tab
+        // results in 2 tables prepended by extension but only the latest one is needed
+        [...tablesAddedByExtension].slice(1).forEach(table => stickerTableContainerElement.removeChild(table))
+    }
+    // normal tab -> normal tab 
+    // and normal tab -> extension tab
+    // doesn't need to be handled
+
+    // Actually one more state: initial (where mutationRecord.oldValue == null)
+    // Initial state happens when if previously user was on sticker search tab then close popup
+    // Currently, from initial state -> extension tab will fall to #2 and the latest (only 1) will be kept
+    // So this case is already handled.
+}
+
 function initialize() {
     let isStickerTabsAppended = false;
-    let chatTabObserver = new MutationObserver((mutationList, observer) => {
-        let stickersContainerDiv = document.querySelector('div[aria-label=Stickers] > div > div > div > div');
-        if (stickersContainerDiv !== null && !isStickerTabsAppended) {
+    let chatTabContainerObserver = new MutationObserver((mutationList, observer) => {
+        let stickersTableObserver = new MutationObserver(stickerTableContainerMutationHandler);
+
+        let stickersTabBar = document.querySelector(
+            'div[aria-label=Stickers] > div > div > div > div');
+        if (stickersTabBar !== null && !isStickerTabsAppended) {
             if (!isStickerTabsAppended) {
                 let element = createStickerTabContainerElement(stickerGroups);
-                stickersContainerDiv.appendChild(element);
+                stickersTabBar.appendChild(element);
                 isStickerTabsAppended = true;
+                observeDataIdOfStickerTableContainer(stickersTableObserver);
             }
         } 
 
-        if (stickersContainerDiv === null) {
+        if (stickersTabBar === null) {
             isStickerTabsAppended = false;
+            stickersTableObserver.disconnect();
         }
     });
     
-    observeForStickerPopup(chatTabObserver);
+    observeForStickerPopup(chatTabContainerObserver);
+}
+
+function observeDataIdOfStickerTableContainer(mutationObserver) {
+    let config = {
+        attributeFilter: ['data-id'],
+        attribute: true,
+        attributeOldValue: true
+    };
+
+    // Observe the div that contains the sticker table div
+    let tableContainerElement = document.querySelector(
+        'div[aria-label=Stickers] > div:nth-child(2) > div');
+    
+    mutationObserver.observe(tableContainerElement, config);
 }
 
 function observeForStickerPopup(mutationObserver) {
@@ -63,7 +163,7 @@ function observeForStickerPopup(mutationObserver) {
     };
     
     // Only watch chat tabs container to avoid too many mutation being detected
-    var chatTabContainer = document.getElementsByClassName('_59v1')[0];
+    let chatTabContainer = document.getElementsByClassName('_59v1')[0];
     if (chatTabContainer) {
         mutationObserver.observe(chatTabContainer, config);
     } else {
