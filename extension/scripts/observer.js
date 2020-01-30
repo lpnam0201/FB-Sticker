5261a1c127ed50818c4fbd94e83374cab5221118
NamLP <lpnam0201@gmail.com>
2020-01-30 09:51:24 +0700

    should not send sticker if chat box has pending message / file to upload

--- extension/scripts/observer.js
+++ extension/scripts/observer.js
@@ -212,21 +212,31 @@ function observeChatToolBar(mutationObserver, chatToolBarElement) {
     mutationObserver.observe(chatToolBarElement, options);
 }
 
+function canSendSticker(dropPanelElement) {
+    // Chat box already has something to send, should not upload extension's sticker
+    let sendButton = dropPanelElement.querySelector('._6gb._6wm4._6987');
+    return sendButton === null;
+}
+
 function onStickerClick(stickerElement) {
     let dropPanelElement = stickerElement.closest('div.uiContextualLayerPositioner._1r_9.uiLayer')
         .previousElementSibling
         .querySelector('._1ia._2sz2');
 
-    fetch(stickerElement.src)
-        .then(res => res.arrayBuffer())
-        .then(buffer => {
-            let chatToolBarElement = dropPanelElement.querySelector('._552n');
-            let chatToolBarObserver = new MutationObserver(chatToolBarMutationHandler);
-            observeChatToolBar(chatToolBarObserver, chatToolBarElement);
+    // Should only upload sticker if chat box is empty (no message, no files)
+    if (canSendSticker(dropPanelElement)) {
+        fetch(stickerElement.src)
+            .then(res => res.arrayBuffer())
+            .then(buffer => {
+                // Upload sticker gif takes a short time, during that time the send button isn't available
+                let chatToolBarElement = dropPanelElement.querySelector('._552n');
+                let chatToolBarObserver = new MutationObserver(chatToolBarMutationHandler);
+                observeChatToolBar(chatToolBarObserver, chatToolBarElement);
 
-            let file = new File([buffer], 'a.gif', { type: 'image/gif' });
-            simulateDragDrop(stickerElement, dropPanelElement, [file]);
-        })
+                let file = new File([buffer], 'a.gif', { type: 'image/gif' });
+                simulateDragDrop(stickerElement, dropPanelElement, [file]);
+            });
+    }
 }
 
 function initialize() {
